696281295e839ebd0c7eef5466bbe93939972e0c
anubhav100rao <anubhav100rao@gmail.com>
2022-02-22 23:45:27 +0530

    project completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import io from 'socket.io-client'
 import './App.css';
 
+const END_POINT = "http://localhost:5000"
+const socket = io.connect(END_POINT)
+
+
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [messages, setMessages] = useState(["Welcome"])
+    const [message, setMessage] = useState("")
+    
+    useEffect(() => {
+        socket.on("message", msg => {
+            setMessages([...messages, msg])
+        })
+    })
+
+    const onChange = (event) => {
+        setMessage(event.target.value)
+    }
+
+    const onFormSubmit = (event) => {
+        event.preventDefault()
+        if(message !== "") {
+            socket.emit("message", message)
+            setMessage("")
+        } else {
+            alert("please add a message")
+        }
+    } 
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Messenger</h1>
+                {messages.length > 0 && 
+                    messages.map((msg, index) => {
+                        return (
+                            <p key={index}> { msg }</p>
+                        )
+                    })
+                }
+                <form onSubmit={ onFormSubmit }>
+                    <input type="text" name="message" 
+                        placeholder='send chat'
+                        value={ message }
+                        onChange = {(event) => {
+                            onChange(event)
+                        }}
+                    />
+                    <button type='submit'> send </button>
+                </form>
+            </header>
+        </div>
+    );
 }
 
 export default App;
